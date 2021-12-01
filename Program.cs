ee0f886362b21d04ca33e1fa00ad0c4baaa98aa9
_futen_ <79382698+brofuten@users.noreply.github.com>
2021-12-01 09:08:07 +0100

    cases-&-switch-in-c-sharp

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Conditions_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+
+
+            Console.Write("Input a number between 1 and 5:  ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            switch (num) {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+                default:
+                    Console.WriteLine("Default");
+                    break;
+            }
+            
+            //wait before closing
+            Console.ReadKey();
+        }
+
+    }
+}
