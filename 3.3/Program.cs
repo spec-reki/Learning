1ab165451349a19651769f56e54c3b8f5bc4e2f9
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-03-26 09:38:42 +0100

    EP 3

---
+++ 3.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result1;
+
+            Console.WriteLine("10 * 2 + 3 = ");
+            result1 = Convert.ToInt32(Console.ReadLine());
+            
+            if(result1 == 23){
+                Console.WriteLine("CORRECT!");
+            } else {
+                Console.WriteLine("You are just stupid son of a bitch!");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
