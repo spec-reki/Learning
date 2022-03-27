4f813b671b8af92b2bfe19a68f9a66dd5cf6dd07
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-03-27 21:07:25 +0200

    EP 4
    Loops - for

---
+++ 4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for (int i = 10; i > 0; i--)
+           {
+               Console.WriteLine(i );
+           }
+
+
+            Console.ReadKey();
+        }
+    }
+}
