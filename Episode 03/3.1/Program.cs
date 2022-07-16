6f6f71d9d692c15f497fe5d43fc4ae0261dca96a
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-07-16 22:14:42 +0200

    Episode

---
+++ Episode 03/3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yo man! I can sell u some lolipops. One is 4$. Give me some money my man.");
+
+            int cash = Convert.ToInt32(Console.ReadLine());
+
+            if(cash < 4)
+            {
+                    Console.WriteLine("That's not enough my n-word.");
+            }
+            else if (cash == 4)
+            {
+                Console.WriteLine("Here is your lolipop");
+            }
+            else
+            {
+                int change = cash - 4;
+                Console.WriteLine("Here is your lolipop bruh, I've got u some chenge. \nYou just earned " + change + "$");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+//      == is equal to
+//      !=  is not equall to
+//      >greater than / <less than
+//      >= greather than or equal to
+//      <= less than or equal to
