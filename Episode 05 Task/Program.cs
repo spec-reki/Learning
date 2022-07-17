ae5696e0bb0ee05204df746cdd41fcf0daeffc96
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-07-17 11:19:28 +0200

    Episode 05 Task complited

---
+++ Episode 05 Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Episode_05_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.Write("How many students are in your class: ");
+
+
+            int studentCount = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Please input the names of the students: ");
+
+
+            string[] students = new string[studentCount];
+
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                students[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("--------------");
+
+            Array.Sort(students);
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+
+
+            // Wait before closing
+            Console.ReadKey();
+        }
+    }
+}
