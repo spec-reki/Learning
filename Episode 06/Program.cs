7dfe072fe00f104a3cff558443e64e998c4b6949
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-07-19 21:21:09 +0200

    Method created

--- Episode 06/Program.cs
+++ Episode 06/Program.cs
@@ -6,8 +6,24 @@ namespace Episode
     {
         static void Main(String[] args)
         {
+
+
+
+
             // Wait before closing
             Console.ReadKey();
+        }
+
+        static void MeetAlien()
+        {
+            Random numberGen = new Random();
+
+            string name =  "M-" + numberGen.Next(10, 9999);
+            int age = numberGen.Next(10, 500);
+
+            Console.WriteLine("Hi, I'm" + name);
+            Console.WriteLine("I'm " + age + " years old.");
+            Console.WriteLine("Oh, and I'm an alien.");
         }
     }
 }
