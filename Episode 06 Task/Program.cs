d587e558e17431abce35cf1aa5711388be5fd46f
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-07-21 22:02:00 +0200

    Task 6 working solution

--- Episode 06 Task/Program.cs
+++ Episode 06 Task/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace My_Awesome_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence: ");
+
+
+            string sentence = Console.ReadLine();
+
+
+            int wordCount = CountWords(sentence);
+
+
+            Console.WriteLine("There are " + wordCount + " words in that sentence.");
+
+
+            // Wait before closing
+            Console.ReadKey();
+        }
+
+
+        static int CountWords (string sentence) {
+
+
+            int wordCount = sentence.Split(' ').Length;
+
+
+            return wordCount;
+        }
+    }
+}
