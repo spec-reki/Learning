3bd39efc919a6da0a60e188695984b3693db245e
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-07-27 18:36:33 +0200

    Creating a class
    Created a wizard class and then i used it to make some things in Program class

--- Episode 07/Program.cs
+++ Episode 07/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Episode_07
+{
+    class Wizard
+    {
+        public string name;
+        public string favouriteSpell;
+        public int spellSlots;
+        public float experience;
+    }
+    
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Wizard wizard01 = new Wizard();
+            wizard01.name = "Gandalf";
+            wizard01.favouriteSpell = "U shall not pass";
+            wizard01.spellSlots = 2;
+            wizard01.experience = 0;
+
+            Console.ReadKey();
+        }
+    }
+}
