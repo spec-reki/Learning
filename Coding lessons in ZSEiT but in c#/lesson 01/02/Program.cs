b621f90abc23315eecbbdbddc6b18f0c324c0f5e
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-10-18 12:59:27 +0200

    Lesson 01 task 02

---
+++ Coding lessons in ZSEiT but in c#/lesson 01/02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace two
+{
+    
+    class Person
+    {
+        public string name;
+        public Person(string _name)
+        {
+            name = _name;
+        }
+        public void TellName()
+        {
+            Console.WriteLine("My name is " + name);
+        }
+    }
+  
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person01 = new Person("Maks");
+
+            person01.TellName();
+
+            Console.ReadKey();
+        }
+    }
+}
