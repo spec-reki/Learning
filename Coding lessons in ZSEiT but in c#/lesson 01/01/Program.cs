6fef941966b6c30097ef115819b0318fe56e6639
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-10-18 12:33:13 +0200

    Lesson 01 task 01

---
+++ Coding lessons in ZSEiT but in c#/lesson 01/01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Maks";
+            Console.WriteLine("Hey, my name is " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
