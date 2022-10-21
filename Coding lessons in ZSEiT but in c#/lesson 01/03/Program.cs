2d17bd7d4b2c1b862e62d27f42b9b1c7ecf261a5
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-10-22 00:18:17 +0200

    task 03

---
+++ Coding lessons in ZSEiT but in c#/lesson 01/03/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace two
+{
+    
+    class Person
+    {
+        public string name;
+        public string lastname;
+        public int age;
+        public string profession;
+        public Person(string _name, string _lastname, int _age, string _profession )
+        {
+            name = _name;
+            lastname = _lastname;
+            age = _age;
+            profession = _profession;
+        }
+        public void TellName()
+        {
+            Console.WriteLine(name + lastname + age + profession);
+        }
+    }
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person01 = new Person("Maks" , "Pokrzywnicki " , 17 , " Grafik");
+            Person person02 = new Person("Bogdan " , "Boner " , 56 , " Egzorcysta");
+            Person person03 = new Person("Krystian " , "Malinowy " , 87 , " Elektryk");
+
+            person02.TellName();
+
+            Console.ReadKey();
+        }
+    }
+}
