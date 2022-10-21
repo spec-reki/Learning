ba1d1dbaaea889536299ea1a97b74415e39b4675
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-10-22 01:37:14 +0200

    LESSON 02 task 01

---
+++ Coding lessons in ZSEiT but in c#/lesson 02/01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Something
+{
+    class Employee
+    {
+        public string name;
+        public string lastname;
+        public string email;
+        public int salary;
+        public static int Count;
+
+        public Employee(string _name, string _lastname, string _email, int _salary)
+        {
+            name = _name;
+            lastname = _lastname;
+            email = _email;
+            salary = _salary;
+
+            Count++;
+        }
+
+        public void GetAnnualSallary()
+        {
+            Console.WriteLine("Roczne wynagrodzenie + " + salary*12);
+        }
+
+        public void ShowEmployeeInformation()
+        {
+            Console.WriteLine("Employee: " + name + "e-mail: " + email + "month salary: " + salary);
+        }
+
+    }
+    
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            
+        }
+    }
+}
