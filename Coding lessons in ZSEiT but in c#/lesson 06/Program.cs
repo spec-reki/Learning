c5cbb55ee002ff98dea00f8ea2180dd3eb19e8ae
spec-reki <realnuosoi@gmail.com>
2022-11-03 09:25:02 +0100

    Tringle

---
+++ Coding lessons in ZSEiT but in c#/lesson 06/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+
+namespace Program
+{
+    class Trojkat
+    {
+        public int a;
+        public int b;
+        public int c;
+
+        public Trojkat(int _a, int _b, int _c)
+        {
+            a = _a;
+            b = _b;
+            c = _c;
+        }
+
+        public void Inicjalizator()
+        {
+            Trojkat trojkat01 = new Trojkat( a, b, c);
+            
+            Console.Write("Podaj długość boku a:");
+            a=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Podaj długość boku b:");
+            b=Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Podaj długość boku c:");
+            c=Convert.ToInt32(Console.ReadLine());
+
+            if(a + b > c)
+            {
+                Console.WriteLine("Z odcinków można zbudować trójkąt!");
+            }
+
+        }
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.ReadKey();
+        }
+    }
+}
+
