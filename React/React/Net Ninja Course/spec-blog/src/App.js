82709ecdaf5af06252288089f1e22e217f93ae09
-reki- <79382698+spec-reki@users.noreply.github.com>
2022-11-13 04:11:52 +0100

    Dynamic Values in Templates

--- React/React/Net Ninja Course/spec-blog/src/App.js
+++ React/React/Net Ninja Course/spec-blog/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+  const title = 'Yo, mr.White';
+  const likes = 420;
+  const github = "https://github.com/spec-reki"
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Components</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+
+        <p>{20}</p>
+        <p>{"Jessie, It's not a wire, it's copper"}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{ Math.random() * 10}</p>
+
+        <a href={github}>Github</a>
       </div>
     </div>
   );
