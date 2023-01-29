9901316a0c0e30323eef41c132d5cdc0bd7f19d5
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-01-29 01:01:02 +0100

    Working on easy scripted conversation

---
+++ C#/Trying to figure out how c# works on my own/Project01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Program
+{
+    class WhoUR
+    {
+        public string name;
+        public Person(string _name)
+            {
+                name = _name;
+            }
+
+        public void Introducing()
+        {
+            Console.Write("Sup bro, what's your name?");
+            Console.ReadLine()
+
+            Console.ReadKey();
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Title = "ReallyCoolTitle";
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowWidth =40;
+            Console.WindowHeight = 25;
+
+            
+
+            
+
+        }
+    }
+}
+
+
