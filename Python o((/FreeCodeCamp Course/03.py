7b2d2809e9e3cd162e1959dda309978ae4ef1e89
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-02-14 11:20:16 +0100

    Start learning python

---
+++ Python o((/FreeCodeCamp Course/03.py
@@ -0,0 +1,9 @@
+character_name = "Arthur"
+character_age = "69"
+isMale = True
+print("This man is named " + character_name + " and he was born, "+ character_age + " years ago.")
+
+character_name = "Anna"
+character_age = "45"
+isMale = False
+print("This man is named " + character_name + " and he was born, "+ character_age + " years ago.")
