70368676826c4577f35661aa25644e920edcc00d
spec-reki <79382698+spec-reki@users.noreply.github.com>
2023-02-15 11:13:34 +0100

    Numbers and imports

---
+++ Python o((/FreeCodeCamp Course/05.py
@@ -0,0 +1,16 @@
+print(2)
+
+print(2 + 2)
+
+print(10 % 3)
+
+my_num = -9
+print(str(my_num) + " is my favorite number")
+
+print(abs(my_num))
+
+print(pow(my_num, 2))
+
+print(max(2,15 , 3))
+
+print(round(5.8))
---
+++ Python o((/FreeCodeCamp Course/06.py
@@ -0,0 +1,8 @@
+from math import *
+
+my_num = -9
+print(floor(3.7))
+
+print(ceil(3.7))
+
+print(sqrt(225))
