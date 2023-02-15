557659c61d8f538ed5307f645fa426327cd5af55
spec-reki <79382698+spec-reki@users.noreply.github.com>
2023-02-15 11:03:34 +0100

    Strings

--- Python o((/FreeCodeCamp Course/03.py
+++ Python o((/FreeCodeCamp Course/03.py
@@ -1,9 +1,9 @@
 character_name = "Arthur"
 character_age = "69"
-isMale = True
+is_Male = True
 print("This man is named " + character_name + " and he was born, "+ character_age + " years ago.")
 
 character_name = "Anna"
 character_age = "45"
-isMale = False
+is_Male = False
 print("This man is named " + character_name + " and he was born, "+ character_age + " years ago.")
---
+++ Python o((/FreeCodeCamp Course/04.py
@@ -0,0 +1,17 @@
+phrase = "I hate python"
+print(phrase.upper().isupper())
+
+print(len(phrase))
+
+print(phrase[0])
+
+print(phrase.index("y"))
+
+print(phrase.replace("I", "We"))
+
+
+'''- I can use also upper() to make all the letters uppercase
+- I can use also title() to make the first letter of each word uppercase
+- I can use also capitalize() to make the first letter of the first word uppercase 
+- I can use also swapcase() to make the uppercase letters lowercase and vice versa 
+- I can use also count() to count the number of times a letter appears in a string'''
