c7eb3759913dfa16abeffee4aaa297ed9c37530d
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-03-14 23:38:21 +0100

    Using information given by user

---
+++ C#/Preapering for my qualifying exam (INF-04)/Basics/Program 02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace program_S02
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            string name;
+
+            Console.WriteLine("Yo man, what's your name?");
+
+            name = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("Hello " + name + "!");
+
+            Console.ReadLine();
+
+        }
+    }
+}
