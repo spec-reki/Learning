c34e2539e4dd9fe8651027e1d699d6f5d3ba6463
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-03-14 23:24:51 +0100

    Progarm 01

---
+++ C#/Preapering for my qualifying exam (INF-04)/Basics/Parogram 01/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I really need to learn C# \nfor my exams :o");
+            Console.ReadKey();
+        }
+    }
+}
