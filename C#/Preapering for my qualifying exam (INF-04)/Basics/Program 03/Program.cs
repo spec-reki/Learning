6d4620cf7b3c54eb5c98e9d09574daaec5fc3037
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-14 20:58:52 +0200

    C# learning

---
+++ C#/Preapering for my qualifying exam (INF-04)/Basics/Program 03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace program{
+    class program{
+        static void Main(string[] args){
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Title = "difference between two points";            
+
+            int a = 10;
+            int b = 20;
+            int c = a - b;
+
+            Console.WriteLine("The difference of {0} and {1} is {2}", a, b, c);
+
+            Console.ReadKey();
+        }
+    }
+}
