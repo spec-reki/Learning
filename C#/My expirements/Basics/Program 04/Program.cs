111d6e3ff4086df1dea91fc44376110470ee66a2
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-15 00:12:07 +0200

    variables 02

---
+++ C#/My expirements/Basics/Program 04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 16;
+            float height = 1.8f;
+            string name = "Jotaro";
+            bool isMarried = false;
+
+            Console.WriteLine("Name of mine is "+name+", I am "+age+" years old, I am "+height+"meters tall and I am not married");
+            Console.ReadKey();
+        }
+    }
+}
