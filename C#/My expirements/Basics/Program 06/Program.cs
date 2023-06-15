46629b427a5b762e7272223fc8d5d90dde7e2ef3
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-16 01:10:03 +0200

    Sum of the two numbers

---
+++ C#/My expirements/Basics/Program 06/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Program{
+    class Program{
+        static void Main(string[] args){
+            
+            int num01;
+            int num02;
+
+            Console.Write("Write a first number: ");
+            num01 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Write a second number: ");
+            num02 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nThe sum of {0} and {1} is: {2}", num01, num02, num01 + num02);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
