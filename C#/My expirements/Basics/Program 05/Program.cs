bbbd550904c83119bffc198c94769beee850faca
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-16 01:06:15 +0200

    ReadLine

---
+++ C#/My expirements/Basics/Program 05/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Program{
+    class Program{
+        static void Main(string[] args){
+            
+            Console.WriteLine("Yo, bitch! What's your name?");
+            string bitchName = Console.ReadLine();
+            Console.WriteLine("Okey, "+bitchName+". Lets cook meth. ;)");
+
+
+            Console.ReadKey();
+        }
+    }
+}
