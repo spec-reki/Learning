f608c4ab55bac283e80b25c0cd9feb3cf3b32ea9
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-25 20:47:31 +0200

    Answer checker

---
+++ C#/My expirements/Basics/Program 08/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program08{
+    class Program{
+        static void Main(string[] args){
+
+            int a;
+
+            Console.WriteLine("What is 2 + 3?");
+            a = Convert.ToInt32(Console.ReadLine());
+
+             if (a == 5){
+                Console.WriteLine("Correct! Good job!");
+             } else {
+                Console.WriteLine("Incorrect! You are dumb!");
+             }
+            
+            Console.ReadKey();
+        }
+    }
+}
