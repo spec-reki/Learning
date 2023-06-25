ffec53c46c963c3f98e7efaa863c4433a765efec
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-25 20:53:24 +0200

    Billionaire checker

---
+++ C#/My expirements/Basics/Program 09/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Program09{
+    class Program{
+        static void Main(string[] args){
+            
+            Console.WriteLine("Yooo! Welcome to billionaire club!\nIf u want to join us, please enter how mach money do u have: ");
+            int money = Convert.ToInt32(Console.ReadLine());
+
+            if (money >= 1000000000){
+                Console.WriteLine("Congrats! U are a billionaire!");
+            }
+            else{
+                Console.WriteLine("Sorry, u are not a billionaire! Go away!");
+            }            
+            
+            Console.ReadKey();
+
+        }
+    }
+}
