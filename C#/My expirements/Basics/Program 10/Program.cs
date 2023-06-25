228a7936419295e1fac3e202fcfe69766c9de093
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-25 21:14:25 +0200

    automatic cash of cinema

---
+++ C#/My expirements/Basics/Program 10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yooo! Welcome to our cinema! \nTickets for new SPIDER-MAN are 10$. \nHow many tickets do you need? ");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+            int price = tickets * 10;
+
+            Console.WriteLine("Okey! You need to pay {0} dollars.", price);
+            int money = Convert.ToInt32(Console.ReadLine());
+
+            if (money < price)
+            {
+                Console.WriteLine("It's not enough.");
+            } else if (money == price){
+                Console.WriteLine("Here are your tickets.");
+            }else{
+                int change = money -price;
+                Console.WriteLine("Here is your ticket and {0} dollars of change.", change);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
