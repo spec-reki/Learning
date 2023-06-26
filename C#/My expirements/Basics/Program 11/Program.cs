1b548ee67745b43b356c78046fe0620c90d94b33
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-26 17:06:31 +0200

    switch statement

---
+++ C#/My expirements/Basics/Program 11/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Program11{
+    class Program{
+        static void Main(string[] args){
+            
+            Console.WriteLine("Input a number between 1 and 5:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            switch (num) {
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("Two");
+                    break;
+                case 3:
+                    Console.WriteLine("Three");
+                    break;
+                case 4:
+                    Console.WriteLine("Four");
+                    break;
+                case 5:
+                    Console.WriteLine("Five");
+                    break;
+                default:
+                    Console.WriteLine("default");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
