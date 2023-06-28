ff46a7a2f4be40bc82ad34472bed9bfb58cf59c8
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-28 23:19:13 +0200

    While loop rolling one dice

--- C#/My expirements/Basics/Program 15/Program.cs
+++ C#/My expirements/Basics/Program 15/Program.cs
@@ -4,13 +4,24 @@ namespace Program14{
     class Program    {
         static void Main(string[] args){
 
-            Conso
-            
-            for (int i = 1; i <= 10; i++)
-            {
-                double result = Math.Pow(2, i);
-                Console.WriteLine(result);
+            Random numberGen = new Random();
+
+            numberGen.Next(0,10);
+
+            int roll = 0;
+            int attempts = 0;
+
+            Console.WriteLine("Press space to roll d10.");
+
+            while(roll != 10){
+                Console.ReadKey();
+
+                roll = numberGen.Next(1, 11);
+                Console.WriteLine("You rolled {0}", roll);
+                attempts++;
             }
+
+            Console.WriteLine("It took you {0} attemps to roll a ten.", attempts);
 
             Console.ReadKey();
 
