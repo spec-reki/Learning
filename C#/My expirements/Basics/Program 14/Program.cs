37c9ed494f9839c1921cde74a5bfa046e3805445
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-06-28 23:00:30 +0200

    Math Class - Power function

---
+++ C#/My expirements/Basics/Program 14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program14{
+    class Program    {
+        static void Main(string[] args){
+
+            for (int i = 1; i <= 10; i++)
+            {
+                double result = Math.Pow(2, i);
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+
+        }            
+    }
+}
