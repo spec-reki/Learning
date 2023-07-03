57503570f03252880b325bc144cd81d5c016fe4c
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-07-03 22:12:16 +0200

    Number outcome out of array

---
+++ C#/My expirements/Basics/Program 18/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Program17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's work on indexes!!\n");
+
+            string[] jojos = {
+                "Jonathan",
+                "Joseph",
+                "Jotaro",
+                "Josuke",
+                "Giorno"
+            };
+
+            for (int i = 0; i < jojos.Length; i++)
+            {
+                int rank = i + 1;
+                Console.WriteLine(rank + ". " +jojos[i]);  
+            }
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
