c5217a48a0c40b440d8726f9d6d02cc3b82ed783
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-07-03 22:03:18 +0200

    Arrays

---
+++ C#/My expirements/Basics/Program 17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's work on indexes!!\n");
+
+            string[] jojos = {
+                "Jonathan",
+                "Joseph",
+                "Jotaro",
+                "Josuke",
+                "Giorno"
+            };
+
+            Console.Write(jojos[2]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
