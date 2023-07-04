09bc3ebf819a5fd99712c723e95d9ab243a05bed
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-07-04 18:15:31 +0200

    School class assigments

---
+++ C#/My expirements/Basics/Program 22/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("How many students are in your class:");
+
+            int classSize = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give me names of your students:");
+
+            string[] klasa = new string[classSize];
+            
+            for (int i = 0; i < classSize; i++)
+            {
+                klasa[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("------------------------------\nHere is your class in correct order:");
+            Array.Sort(klasa);
+
+            for (int i = 0; i < classSize; i++)
+            {
+                Console.Write(i+1 + ". ");
+                Console.WriteLine(klasa[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
