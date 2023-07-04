52927de73bef1c86a3f64181aec0cb9975bdc3a2
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-07-04 16:28:41 +0200

    Adding data into array from console

---
+++ C#/My expirements/Basics/Program 19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Program19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] jojos = new string[4];
+
+            Console.WriteLine("Type in four jojos: ");
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write(i+1 + ". ");
+                jojos[i] = Console.ReadLine();
+            }
+            
+            Console.WriteLine("Bro.{0}; Bruh.{1}", jojos[0], jojos[1]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
