efb576b15accc417155bbb1b1c0f253a46fa70e0
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-07-04 16:33:49 +0200

    Alphabetical order

---
+++ C#/My expirements/Basics/Program 20/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Program19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] jojos = new string[4];
+
+            Console.WriteLine("Type in four jojos: ");
+
+            for (int i = 0; i < jojos.Length; i++)
+            {
+                Console.Write(i+1 + ". ");
+                jojos[i] = Console.ReadLine();
+            }
+            
+            Console.WriteLine("\nHere are those mf by alphabetical order: ");
+
+            Array.Sort(jojos);
+
+            for (int i = 0; i < jojos.Length; i++)
+            {
+                Console.WriteLine(jojos[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
