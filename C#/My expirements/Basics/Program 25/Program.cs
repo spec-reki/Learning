f6007c83c699613e76c1260ee1df6ebbbc1faf9d
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-07-05 23:33:59 +0200

    even and uneven numbers out of multiplying

---
+++ C#/My expirements/Basics/Program 25/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the number that u want to power");
+            int result = Multiply(Convert.ToInt32(Console.ReadLine()),Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("--------------------");
+            
+
+            if (result % 2 == 0)
+            {
+                Console.WriteLine("Power of your number is: {0}",result);
+                Console.WriteLine("{0} is an even number!",result);
+            } else {
+                Console.WriteLine("Power of your number is: {0}",result);
+                Console.WriteLine("{0} is an uneven number!",result);
+            }
+
+
+            Console.ReadKey();
+
+        }
+
+        static int Multiply(int number01, int number02){
+            int result = number01 * number02;
+            return result;
+        }
+    }
+}
