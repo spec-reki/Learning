3bfe4d231d74405e6fc21a7a22231454f24c9a99
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-08-13 21:56:47 +0200

    classes basics

---
+++ C#/My expirements/Basics/Program 26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program26{
+
+    class Cat{
+        public string name;
+        public int age;
+
+        public void Meow(){
+            Console.WriteLine(name + " says meow!");
+        }
+    }
+    class Program{
+        static void Main(string[] args)
+        {
+            Cat cat01 = new Cat();
+
+            cat01.name = "Cat Zyzz";
+            cat01.age = 1;
+
+            cat01.Meow();
+
+            Console.ReadKey();
+        }
+    }
+}
