b875536f71c290081969c27dd32776a51e32b69f
-reki- <79382698+spec-reki@users.noreply.github.com>
2023-10-29 16:32:14 +0100

    wall jump work

--- Godot/scenes/main_character.gd
+++ Godot/scenes/main_character.gd
@@ -9,34 +9,34 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-    #Animations
-    if (velocity.x > 1 || velocity.x < -1):
-        sprite_2d.animation = "runanim"
-    else:
-        sprite_2d.animation = "default"
-    
-    # Add the gravity.
-    if not is_on_floor():
-        velocity.y += gravity * delta
-        sprite_2d.animation = "jumpanim"
+	#Animations
+	if (velocity.x > 1 || velocity.x < -1):
+		sprite_2d.animation = "runanim"
+	else:
+		sprite_2d.animation = "default"
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		sprite_2d.animation = "jumpanim"
 
-    # Handle Jump.
-    if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
-        
-    if Input.is_action_just_pressed("jump") and is_on_wall():
-        velocity.y = JUMP_VELOCITY
-        
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("jump") and is_on_wall():
+		velocity.y = JUMP_VELOCITY
+		
 
-    # Get the input direction and handle the movement/deceleration.
-    # As good practice, you should replace UI actions with custom gameplay actions.
-    var direction = Input.get_axis("left", "right")
-    if direction:
-        velocity.x = direction * SPEED
-    else:
-        velocity.x = move_toward(velocity.x, 0, 10)
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 10)
 
-    move_and_slide()
+	move_and_slide()
 
-    var isLeft = velocity.x < 0
-    sprite_2d.flip_h = isLeft
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
