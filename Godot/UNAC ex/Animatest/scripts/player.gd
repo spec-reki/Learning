f9fa1993c1c721e99b38ee2303c219bf73394b77
-reki- <79382698+spec-reki@users.noreply.github.com>
2025-03-31 20:12:19 +0200

    starting 1st tutorial

--- Godot/UNAC ex/Animatest/scripts/player.gd
+++ Godot/UNAC ex/Animatest/scripts/player.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 800.0
+const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -13,6 +13,9 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		
+	if Input.is_action_just_pressed("move_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
@@ -31,8 +34,6 @@ func _physics_process(delta):
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
-		
-	
 	
 	
 	if direction:
